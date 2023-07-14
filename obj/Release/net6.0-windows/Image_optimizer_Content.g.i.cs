4c64d5ec0f0b7014bcc3a730f07311e15593365b
Simran Verma <simranv@mcnsolutions.net>
2023-07-14 14:37:16 +0530

    update XAML file

--- obj/Release/net6.0-windows/Image_optimizer_Content.g.i.cs
+++ obj/Release/net6.0-windows/Image_optimizer_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imgicon.ico")]
 
 
